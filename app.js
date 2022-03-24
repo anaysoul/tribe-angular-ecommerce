ebd51f41519e081f74e3d4a0a35c622310159be2
anaysoul <soulyana_lakew@outlook.com>
2022-03-23 21:23:15 -0400

    installing mongoose and connect to MongoDB database

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { ServerApiVersion } = require('mongodb');
 
 require('dotenv/config');
-
 const api = process.env.API_URL;
 
 // Middleware
@@ -24,6 +25,20 @@ app.post(`${api}/products`, (req, res) => {
   console.log(newProduct);
   res.send(newProduct);
 });
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+    dbName: 'tribe-database',
+  })
+  .then(() => {
+    console.log('Database connection is ready....');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log(api);
