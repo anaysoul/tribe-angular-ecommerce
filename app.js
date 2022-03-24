97cce2e87b909bcc426d9ba4f6500382538f9951
anaysoul <soulyana_lakew@outlook.com>
2022-03-23 20:47:54 -0400

    Create First API call & parsing json data

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
-// http://localhost:3000/api/v1/products
-app.get(api + '/products', (req, res) => {
-  res.send('Hello API!');
+// Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 app.listen(3000, () => {
