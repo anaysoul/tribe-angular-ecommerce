72f7a51ad79b659df5caa34442b3463b822ce297
anaysoul <soulyana_lakew@outlook.com>
2022-03-24 23:41:30 -0400

    post/register a new user REST API

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,37 @@
-const {User} = require('../models/user');
+const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find();
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
-})
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
 
-module.exports =router;
+router.post('/', async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) {
+    return res.status(400).send('The user cannot be registered!');
+  }
+
+  res.send(user);
+});
+
+module.exports = router;
