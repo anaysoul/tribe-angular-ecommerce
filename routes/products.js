ad8ede00d57732d80c477d15845ec308bdaf31d1
anaysoul <soulyana_lakew@outlook.com>
2022-03-24 21:59:11 -0400

    delete a product rest api and validate id

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const { Product } = require('../models/product');
 const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get(`/`, async (req, res) => {
   const productList = await Product.find()
@@ -53,6 +54,9 @@ router.post(`/`, async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send('Invalid product id!');
+  }
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send('Invalid category');
 
@@ -79,6 +83,24 @@ router.put('/:id', async (req, res) => {
   }
 
   res.send(product);
+});
+
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
+    .then((product) => {
+      if (product) {
+        return res
+          .status(200)
+          .json({ success: true, message: 'The product is deleted.' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Product not found.' });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
 });
 
 module.exports = router;
