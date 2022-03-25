3877b95f8bde995b4b62dec67351ceb63518843d
anaysoul <soulyana_lakew@outlook.com>
2022-03-24 23:55:58 -0400

    get user and list of users excluding passwords

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 
+// all users
 router.get(`/`, async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select('-passwordHash');
 
   if (!userList) {
     res.status(500).json({ success: false });
@@ -12,6 +13,19 @@ router.get(`/`, async (req, res) => {
   res.send(userList);
 });
 
+// single user
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id).select('-passwordHash');
+  //
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: 'The user with the given ID was not found!' });
+  }
+  res.status(200).send(user);
+});
+
+// register user
 router.post('/', async (req, res) => {
   let user = new User({
     name: req.body.name,
